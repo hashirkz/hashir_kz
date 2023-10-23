9ab4f6be584d4233dbc8e8c55d3b431f062b2427
hashirkz <hashirxkhan1@gmail.com>
2023-10-23 00:33:06 -0400

    initial commit for my website

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+
+    <link rel="stylesheet" href="grape_juice.css">
+</head>
+<body>
+    <div class="container">
+    <a href="/" class="name"><h1 class="name">hashir ._. kz</h1></a>
+
+    <ul class="navbar">
+        <li class="social">
+            <a href="https://github.com/hashirkz">
+                <div class="card-container">
+                    <img class="hand-drawn-1" src="./icons/github_1.svg" alt="github">
+                    <img class="hand-drawn-2" src="./icons/github_2.svg">
+                </div>
+            </a>
+        </li>
+
+        <li class="social">
+            <a href="https://discordapp.com/users/519544642081914891">
+                <div class="card-container">
+                    <img class="hand-drawn-1" src="./icons/discord_1.svg" alt="discord">
+                    <img class="hand-drawn-2" src="./icons/discord_2.svg">
+                </div>
+            </a>
+        </li>
+
+        <li class="social">
+            <a href="https://monkeytype.com/profile/sleepyzzzz">
+                <div class="card-container">
+                    <img class="hand-drawn-1" src="./icons/monkeytype_1.svg" alt="monkeytype">
+                    <img class="hand-drawn-2" src="./icons/monkeytype_2.svg">
+                </div>
+            </a>
+        </li>
+
+        <li class="social">
+            <a href="https://vsco.co/xxxoxoxo7/gallery">
+                <div class="card-container">
+                    <img class="hand-drawn-1" src="./icons/vsco_1.svg" alt="vsco">
+                    <img class="hand-drawn-2" src="./icons/vsco_2.svg">
+                </div>
+            </a>
+        </li>
+
+    </ul>
+
+    </div>
+    
+</body>
+</html>
